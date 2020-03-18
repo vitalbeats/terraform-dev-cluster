2516034bb6b314584a3fa597be936bded71cd029
Stephen Badger <stephen.badger@vitalbeats.com>
2020-03-18 18:01:27 +0000

    Use terraform-eks-cluster to deploy dev

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "cluster" {
   source  = "vitalbeats/cluster/eks"
-  version = "0.1.0-alpha"
-
+  version = "0.1.0-beta.3"
+  
   cluster-name = "scaut-v2-dev"
+  cluster-region = "eu-west-1"
 }
