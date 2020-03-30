12a0d407d3de5a59020e0257178626239f8f38aa
Stephen Badger <stephen.badger@vitalbeats.com>
2020-03-30 18:20:15 +0100

    Deploy cluster, and store kubeconfig
    The kubeconfig doesn't contain any sensitive auth details in it, as auth 
    is provided by the IAM authenticator module.

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "cluster" {
   source  = "vitalbeats/cluster/eks"
-  version = "0.1.0-beta.3"
-  
-  cluster-name = "scaut-v2-dev"
+  version = "0.1.0-beta.6"
+
+  cluster-name   = "scaut-v2-dev"
   cluster-region = "eu-west-1"
 }
