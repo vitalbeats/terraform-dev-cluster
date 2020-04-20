757619765a374cf0eec69423a802b41e74f4f961
Stephen Badger <stephen.badger@vitalbeats.com>
2020-04-20 18:47:13 +0100

    Deploy new dev cluster

--- main.tf
+++ main.tf
@@ -1,7 +1,29 @@
+terraform {
+  backend "s3" {
+    bucket = "vitalbeats-terraform-state"
+    dynamodb_table = "vitalbeats-terraform-state-lock"
+    key    = "terraform-eks/dev"
+    region = "eu-west-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  version = "~> 2.0"
+  region = "eu-west-1"
+}
+
+provider "local" {
+  version = "~> 1.4"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
 module "cluster" {
   source  = "vitalbeats/cluster/eks"
-  version = "0.1.0-beta.6"
+  version = "0.1.0-beta.9"
 
   cluster-name   = "scaut-v2-dev"
-  cluster-region = "eu-west-1"
 }
--- terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "vitalbeats-terraform-state"
-    dynamodb_table = "vitalbeats-terraform-state-lock"
-    key    = "terraform-eks/dev"
-    region = "eu-west-1"
-    encrypt = true
-  }
-}
