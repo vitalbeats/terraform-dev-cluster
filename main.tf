cadd62890cf2fd8b9fdd14bd6957229afa4803a2
Stephen Badger <stephen.badger@vitalbeats.com>
2020-12-14 13:56:36 +0000

    SCAUT-922: Adds core EKS monitors

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ provider "datadog" {
 
 module "cluster" {
   source  = "vitalbeats/cluster/eks"
-  version = "0.1.0-beta.46"
+  version = "0.1.0-beta.48"
 
   cluster-name       = "scaut-v2-dev"
   ec2-ssh-key        = "stephen.badger"
