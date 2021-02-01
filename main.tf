3ff6584474c31a810ecf7d72bb3b0412e3563ba0
Stephen Badger <stephen.badger@vitalbeats.com>
2021-02-01 16:29:45 +0000

    Make restarting pod  monitor less sensitive

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ provider "datadog" {
 
 module "cluster" {
   source  = "vitalbeats/cluster/eks"
-  version = "0.1.0-beta.52"
+  version = "0.1.0-beta.53"
 
   cluster-name       = "scaut-v2-dev"
   ec2-ssh-key        = "stephen.badger"
