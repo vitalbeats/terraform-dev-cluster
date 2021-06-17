25a6953b3598e551c4c83e16ce9c0aa365cab6d3
Stephen Badger <stephen.badger@vitalbeats.com>
2021-06-17 22:01:21 +0100

    Upgrade to Kubernetes 1.20

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ provider "datadog" {
 
 module "cluster" {
   source  = "vitalbeats/cluster/eks"
-  version = "0.2.0-beta.7"
+  version = "0.2.0-beta.10"
 
   cluster-name       = "scaut-v2-dev"
   ec2-ssh-key        = "stephen.badger"
